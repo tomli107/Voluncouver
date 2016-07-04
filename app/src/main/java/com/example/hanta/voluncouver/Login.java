18ab2101200fa11c3e3995432b3cf72e64ffba56
tomli107 <tomli9999@hotmail.com>
2016-07-04 15:00:38 -0700

    hi

--- app/src/main/java/com/example/hanta/voluncouver/Login.java
+++ app/src/main/java/com/example/hanta/voluncouver/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +25,7 @@ public class Login extends ActionBarActivity {
                     startActivity(startIntent);
                 }
             });
+        Layout mainpg = new
 
     }
 
